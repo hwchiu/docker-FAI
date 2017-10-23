e3586b8182528db406abb9e541c92a8ba93495a3
Hung-Wei Chiu <hwchiu@linkernetworks.com>
2017-10-23 16:27:39 +0000

    execute dhcpcd background

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-CONFIG_FILE=/etc/dhcp/dhcpd.conf;
-exec dhcpd -user dhcpd -group dhcpd -f -4 -pf /run/dhcp-server/dhcpd.pid -cf /etc/dhcp/dhcpd.conf FAIbr0
+exec dhcpd -user dhcpd -group dhcpd -f -4 -pf /run/dhcp-server/dhcpd.pid -cf /etc/dhcp/dhcpd.conf FAIbr0 &

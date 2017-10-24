14ba79b9f2f7a44fba9799c99bc227aee05214dd
hwchiu <hwchiu@linkernetworks.com>
2017-10-24 07:28:33 +0000

    change to bash

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+mkdir /tmp/iso
 mount -oloop,ro /tmp/${IMAGEISO} /tmp/iso
 
 cp /tmp/iso/casper/{vmlinuz.efi,initrd.lz} /install/tftpboot/${IMAGE}/
 mkdir /${IMAGE}
 cp -R /tmp/iso/* /tmp/iso/.disk /${IMAGE}/
-#umount /tmp/iso
+umount /tmp/iso
 echo "/${IMAGE}           *(ro,sync,no_wdelay,insecure_locks,no_root_squash,insecure,no_subtree_check)" >> /etc/exports
-#rm -rf /tmp/${IMAGEISO}
+rm -rf /tmp/${IMAGEISO}
 
 service rpcbind restart
 exec dhcpd -user dhcpd -group dhcpd -f -4 -pf /run/dhcp-server/dhcpd.pid -cf /etc/dhcp/dhcpd.conf FAIbr0 &

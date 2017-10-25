b590f3fe9e0ba0c21e7658ea8c00b872576e1f00
hwchiu <hwchiu@linkernetworks.com>
2017-10-25 11:25:59 +0800

    install FAI adn setup default configs

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ then
 fi
 INTERFACE=$1
 sed -i -e "s/FAIbr0/$INTERFACE/g" "/etc/default/isc-dhcp-server"
+myip=$(ip a s $dev|sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}'|head -1)
+sed -i -e "s/$myip\/24/\*/" /etc/exports
 service rpcbind restart
 service isc-dhcp-server restart
 service xinetd restart
--- setup.sh
+++ setup.sh
@@ -1,65 +1,20 @@
 #!/usr/bin/env bash
-IMAGE=ubuntu-16.04.3-desktop-amd64
-IMAGEISO=${IMAGE}.iso
-apt-get update
-apt-get -y install isc-dhcp-server \
-    tftpd \
-    xinetd \
-    nfs-kernel-server
-
-
-#Create Directory/Files
-
-##Tftp Server
-mkdir -p /install/tftpboot/${IMAGE}
-##Loop Mount
-mkdir /tmp/iso
-##NFS Server
-mkdir /${IMAGE}
-##DHCP
-touch /var/lib/dhcp/dhcpd.leases
-#PXE Linux
-mkdir /install/tftpboot/pxelinux.cfg
-
-#Copy files
-
+#Copy Files
 mv "/tmp/tftp" "/etc/xinetd.d/tftp"
 mv "/tmp/dhcpd.conf" "/etc/dhcp/dhcpd.conf"
 mv "/tmp/isc-dhcp-server" "/etc/default/isc-dhcp-server"
-mv "/tmp/pxelinux.0" "/install/tftpboot/pxelinux.0"
-mv "/tmp/libcom32.c32" "/install/tftpboot/libcom32.c32"
-mv "/tmp/libutil.c32" "/install/tftpboot/libutil.c32"
-mv "/tmp/ldlinux.c32" "/install/tftpboot/ldlinux.c32"
-mv "/tmp/vesamenu.c32" "/install/tftpboot/vesamenu.c32"
-mv "/tmp/default" "/install/tftpboot/pxelinux.cfg/default"
-mv "/tmp/run.sh" "~ubuntu/run.sh"
-
-#Change String
-sed -i -e "s/IMAGE/$IMAGE/g" "/install/tftpboot/pxelinux.cfg/default"
-
-#Change Permission
-##TFTP Server
-chmod -R 777 /install/tftpboot
-chown -R nobody /install/tftpboot
-
-##DHCP
-chown root:dhcpd /var/lib/dhcp /var/lib/dhcp/dhcpd.leases
-chmod 775 /var/lib/dhcp
-chmod 664 /var/lib/dhcp/dhcpd.leases
 
 
-#Donwload Images
-cd /tmp/
-wget -q http://releases.ubuntu.com/16.04/${IMAGEISO}
+#Install FAI
+wget -O - https://fai-project.org/download/074BCDE4.asc | apt-key add -
+echo "deb http://fai-project.org/download stretch koeln" > /etc/apt/sources.list.d/fai.list
+apt-get update
+apt-get install fai-quickstart
 
-#Mount Loop Devices and get vmlinuz.efim initrd.lz and save them into TFTP server
-mount -oloop,ro /tmp/${IMAGEISO} /tmp/iso
-cp /tmp/iso/casper/{vmlinuz.efi,initrd.lz} /install/tftpboot/${IMAGE}/
+sed -i -e 's/^#deb/deb/' /etc/fai/apt/sources.list
+sed -i -e 's/#LOGUSER/LOGUSER/' /etc/fai/fai.conf
 
-#Copy File into NFS Servrer
-cp -R /tmp/iso/* /tmp/iso/.disk /${IMAGE}/
-umount /tmp/iso
-#Add entry for NFS to export.
-echo "/${IMAGE}           *(ro,sync,no_wdelay,insecure_locks,no_root_squash,insecure,no_subtree_check)" >> /etc/exports
-rm -rf /tmp/${IMAGEISO}
+fai-setup -v
 
+cp -a /usr/share/doc/fai-doc/examples/simple/* /srv/fai/config/
+cp /usr/share/doc/fai-doc/examples/etc/dhcpd.conf /etc/dhcp/
